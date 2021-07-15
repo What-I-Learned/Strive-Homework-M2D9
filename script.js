cefb1c4a1749dad62e1621b26582cb0ef035d853
js25s <petrauskas.justas@gmail.com>
2021-07-15 17:40:38 +0200

    nothing new

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function addBadges(){
     let musicCards = document.querySelectorAll(".music-card")
     for(let card of musicCards){
         let badge = document.createElement("div")
-        badge.classList.add("genre-badge")
+        badge.classList.add("genre-badge","shadow-sm")
         badge.innerText = card.querySelector(".genre").innerText
         card.append(badge)
     }
